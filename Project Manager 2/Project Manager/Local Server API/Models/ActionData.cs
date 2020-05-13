a9f6cd59a702f403010a729f24eed30e05c25766
Elmassaoudi Yassine <massaoudi.yass@gmail.com>
2020-05-13 22:33:57 +0000

    made the ActionData a model for easier parsing to/from JSON

---
+++ Project Manager 2/Project Manager/Local Server API/Models/ActionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace Local_Server_API.Models
+{
+    public class ActionData
+    {
+        public string FromBody { get; set; }
+        public List<string[]> FromURI { get; set; }
+        public string FromRoute { get; set; }
+    }
+}
