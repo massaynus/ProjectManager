9e5b1d368a879ecd7b4e430ba5759124b1968869
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-05-16 00:25:02 +0000

    made a task details UC

---
+++ Project Manager 2/Project Manager/Windows CLient/UserControls/TaskDetails.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WCM = Windows_CLient.Models;
+
+namespace Windows_CLient.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TaskDetails.xaml
+    /// </summary>
+    public partial class TaskDetails : UserControl
+    {
+        public TaskDetails()
+        {
+            InitializeComponent();
+        }
+
+
+        public WCM.Task Task
+        {
+            get { return (WCM.Task)GetValue(TaskProperty); }
+            set { SetValue(TaskProperty, value); }
+        }
+
+        public static readonly DependencyProperty TaskProperty =
+            DependencyProperty.Register("Task", typeof(WCM.Task), typeof(TaskDetails), new PropertyMetadata(new WCM.Task()));
+
+    }
+}
