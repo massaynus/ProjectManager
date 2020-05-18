3a54b21f98ae4efd636b4fe25e754bc4ffd8986e
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-05-18 12:12:55 +0000

    Just renamed the folder

---
+++ Project Manager 2/Project Manager/Windows CLient/Models/RelayCommand/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Windows_CLient
+{
+    class RelayCommand : ICommand
+    {
+        Action action;
+        Action<object> parametrizedAction;
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        public RelayCommand(Action<object> action)
+        {
+            parametrizedAction = action;
+        }
+
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public virtual void Execute(object parameter)
+        {
+            if (action != null)
+            {
+                action();
+                return;
+            }
+            if (parametrizedAction != null)
+            {
+                parametrizedAction(parameter);
+                return;
+            }
+        }
+    }
+}
