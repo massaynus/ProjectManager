7fd559267c8be53bcb755e7f04664f8f96668a25
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-05-21 08:58:54 +0000

    Naming convention change

---
+++ Project Manager 2/Project Manager/Windows CLient/Models/RelayCommand/RelayCommandAsync.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Windows_CLient
+{
+    class RelayCommandAsync : ICommand
+    {
+        Func<Task> func;
+
+        public RelayCommandAsync(Func<Task> func)
+        {
+            this.func = func;
+        }
+
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        public bool CanExecute(object parameter) => true;
+
+        [STAThread]
+        public async void Execute(object parameter)
+        {
+            if (func != null)
+            {
+                await func();
+            }
+            return;
+        }
+    }
+}
