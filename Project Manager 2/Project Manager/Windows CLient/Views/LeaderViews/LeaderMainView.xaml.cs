466db5500743d7386b1f542fdb1ad3ec6a956c78
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-05-26 08:18:43 +0100

    Basic Window layout

--- Project Manager 2/Project Manager/Windows CLient/Views/LeaderViews/LeaderMainView.xaml.cs
+++ Project Manager 2/Project Manager/Windows CLient/Views/LeaderViews/LeaderMainView.xaml.cs
@@ -23,5 +23,10 @@ namespace Windows_CLient.Views.LeaderViews
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
