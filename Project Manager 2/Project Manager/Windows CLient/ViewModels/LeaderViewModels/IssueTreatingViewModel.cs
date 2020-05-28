7f1e9bb85f744e965b36d7d2fde07d9c1b8ca6e7
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-05-28 15:36:41 +0100

    => Added command for getting Issue details (User / Task) => Tweaked GetIssues command => improved SelectedIssue property

--- Project Manager 2/Project Manager/Windows CLient/ViewModels/LeaderViewModels/IssueTreatingViewModel.cs
+++ Project Manager 2/Project Manager/Windows CLient/ViewModels/LeaderViewModels/IssueTreatingViewModel.cs
@@ -13,18 +13,37 @@ namespace Windows_CLient.ViewModels
 {
     public class IssueTreatingViewModel : BaseViewModel
     {
-        //TODO: make it get the Issueer and task info
+        private Issue _selectedIssue;
         public IssueTreatingViewModel()
         {
             MarkAsSolved = new RelayCommandAsync(markAsSolved);
+            GetIssueInfo = new RelayCommandAsync(getIssueInfo);
             GetIssues = new RelayCommandAsync(getIssues);
+
             GetIssues.Execute(null);
+            GetIssueInfo.Execute(null);
         }
 
-        public Issue SelectedIssue { get; set; }
-        public ObservableCollection<Issue> Issues { get; set; }
+        public Issue SelectedIssue
+        { 
+            get => _selectedIssue;
+            set
+            {
+                if (value != _selectedIssue)
+                {
+                    _selectedIssue = value;
+                    OnPropertyChanged(nameof(SelectedIssue));
+                    GetIssueInfo.Execute(null);
+                }
+            } 
+        }
+        public Task IssueTask { get => (SelectedIssue is null) ? null : SelectedIssue.IssueTask; }
+        public User Issuer { get; set; }
+
+        public ObservableCollection<Issue> Issues { get; set; } = new ObservableCollection<Issue>();
         public ICommand MarkAsSolved { get; set; }
         public ICommand GetIssues { get; set; }
+        public ICommand GetIssueInfo { get; set; }
         public async TT.Task markAsSolved()
         {
             SelectedIssue.isSolved = true;
@@ -47,16 +66,48 @@ namespace Windows_CLient.ViewModels
             if (res.IsSuccessStatusCode)
             {
                 Issues = JsonConvert.DeserializeObject<ObservableCollection<Issue>>(await res.Content.ReadAsStringAsync());
-                OnPropertyChanged(nameof(Issues));
                 if (Issues.Count > 0)
                 {
                     SelectedIssue = Issues[0];
                 }
+                OnPropertyChanged(nameof(Issues));
             }
             else
             {
                 MessageBox.Show($"Status Code:  {res.StatusCode}\n{res.ReasonPhrase}", "Oops", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
+        }
+        public async TT.Task getIssueInfo()
+        {
+            if (SelectedIssue is null) return;
+
+            if (SelectedIssue.IssueTask is null)
+            {
+                var res = await APIClient.client.GetAsync($"Tasks/{SelectedIssue.Task}");
+                if (res.IsSuccessStatusCode)
+                {
+                    SelectedIssue.IssueTask = JsonConvert.DeserializeObject<Task>(await res.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show($"Status code: {res.StatusCode}\n{res.ReasonPhrase}", "Oops!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+
+            if (SelectedIssue.IssuerObj is null)
+            {
+                var res = await APIClient.client.GetAsync($"Users/{SelectedIssue.Issuer}");
+                if (res.IsSuccessStatusCode)
+                {
+                    SelectedIssue.IssuerObj = JsonConvert.DeserializeObject<User>(await res.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    MessageBox.Show($"Status code: {res.StatusCode}\n{res.ReasonPhrase}", "Oops!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+
+            OnPropertyChanged(nameof(SelectedIssue));
         }
     }
 }
