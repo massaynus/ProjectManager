108193564907b5d9e164eef95aff33bdc45e195a
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-05-30 17:58:18 +0100

    Made Managers Main View Model for Navigation

--- Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/ManagerMainViewModel.cs
+++ Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/ManagerMainViewModel.cs
@@ -3,11 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Windows_CLient.Views.ManagerViews.Pages;
 
 namespace Windows_CLient.ViewModels
 {
     public class ManagerMainViewModel : BaseViewModel
     {
-        //TODO: complete the ManagerMainViewModel
+        public ManagerMainViewModel()
+        {
+            GotoStatisitcs = new RelayCommand(gotoStatisitcs);
+            GotoProjects = new RelayCommand(gotoProjects);
+            GotoPaiements = new RelayCommand(gotoPaiements);
+            GotoTeams = new RelayCommand(gotoTeams);
+            GotoWorkers = new RelayCommand(gotoWorkers);
+        }
+        public Page CurrentPage { get; set; }
+
+        public ICommand GotoStatisitcs { get; set; }
+        public ICommand GotoProjects { get; set; }
+        public ICommand GotoPaiements { get; set; }
+        public ICommand GotoTeams { get; set; }
+        public ICommand GotoWorkers { get; set; }
+
+        public void gotoStatisitcs()
+        {
+            CurrentPage = new StatisticsPage();
+            OnPropertyChanged(nameof(CurrentPage));
+        }
+        public void gotoProjects()
+        {
+            CurrentPage = new ProjectsPage();
+            OnPropertyChanged(nameof(CurrentPage));
+        }
+        public void gotoPaiements()
+        {
+            CurrentPage = new PaiementsPage();
+            OnPropertyChanged(nameof(CurrentPage));
+        }
+        public void gotoTeams()
+        {
+            CurrentPage = new TeamsPage();
+            OnPropertyChanged(nameof(CurrentPage));
+        }
+        public void gotoWorkers()
+        {
+            CurrentPage = new WorkersPage();
+            OnPropertyChanged(nameof(CurrentPage));
+        }
+
+
     }
 }
