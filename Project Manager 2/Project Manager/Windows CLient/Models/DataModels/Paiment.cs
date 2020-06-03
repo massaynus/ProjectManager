c5270676ad0e83df4adf5d0b5a57b8610c1430f0
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-06-03 23:01:49 +0100

    update Paiment data model

--- Project Manager 2/Project Manager/Windows CLient/Models/DataModels/Paiment.cs
+++ Project Manager 2/Project Manager/Windows CLient/Models/DataModels/Paiment.cs
@@ -4,8 +4,15 @@ namespace Windows_CLient.Models
     public partial class Paiment
     {
         public int PaymentID { get; set; }
-        public int? Project { get; set; }
-        public decimal? Amount { get; set; }
-        public DateTime? Date { get; set; }
+        public string PaymentDescription { get; set; }
+        public string SenderFullName { get; set; }
+        public int? SenderID { get; set; }
+        public string RecieverFullName { get; set; }
+        public int? RecieverID { get; set; }
+        public decimal Amount { get; set; }
+        public bool? isSalary { get; set; }
+        public bool? isProjectPaiement { get; set; }
+        public int? ProjectID { get; set; }
+        public DateTime Date { get; set; }
     }
 }
