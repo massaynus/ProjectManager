fdf543a5ebb9fda25c128447cf619d1c5ec810b4
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-06-03 13:58:57 +0100

    Added Handlers for API calls

--- Project Manager 2/Project Manager/Windows CLient/Models/APIClient/APIClient.cs
+++ Project Manager 2/Project Manager/Windows CLient/Models/APIClient/APIClient.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
+using TT = System.Threading.Tasks;
 using Windows_CLient.Models;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Windows_CLient
 {
@@ -19,6 +21,50 @@ namespace Windows_CLient
         public static User User { get; set; }
 
         public static StringContent GetStringContent(object obj) => new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+
+       /// <summary>
+       /// GET /DELETE calls handler
+       /// </summary>
+       /// <param name="action">Enum</param>
+       /// <param name="controller">Enum</param>
+       /// <param name="id"></param>
+       /// <exception cref="InvalidActionException"/>
+       /// <returns>JSON response</returns>
+        public static async TT.Task<string> MakeApiCall(Action action, Controller controller, string id = "")
+        {
+            HttpResponseMessage res;
+            if (action == Action.GET)
+                res = await client.GetAsync($"{controller}/{id}");
+            else if (action == Action.DELETE)
+                res = await client.DeleteAsync($"{controller}/{id}");
+            else throw new InvalidActionException("Provide either GET or DELETE actions");
+
+            if (res.IsSuccessStatusCode) return await res.Content.ReadAsStringAsync();
+            else MessageBox.Show($"Status Code: {res.StatusCode}\n{res.ReasonPhrase}"); return "";
+        }
+
+        /// <summary>
+        /// PUT / POST calls handler
+        /// </summary>
+        /// <param name="action">Enum</param>
+        /// <param name="controller">Enum</param>
+        /// <param name="id"></param>
+        /// <param name="data">Data</param>
+        /// <exception cref="InvalidActionException"/>
+        /// <returns>JSON response</returns>
+        public static async TT.Task<string> MakeApiCall(Action action, Controller controller, string id = "", object data = null)
+        {
+            HttpResponseMessage res;
+            if (action == Action.POST)
+                res = await client.PostAsync($"{controller}/{id}", GetStringContent(data));
+            else if (action == Action.PUT)
+                res = await client.PutAsync($"{controller}/{id}", GetStringContent(data));
+            else throw new InvalidActionException("Provide either GET or DELETE actions");
+
+            if (res.IsSuccessStatusCode) return await res.Content.ReadAsStringAsync();
+            else MessageBox.Show($"Status Code: {res.StatusCode}\n{res.ReasonPhrase}"); return "";
+        }
+
 
         public static void initClient()
         {
@@ -45,7 +91,6 @@ namespace Windows_CLient
             Test,
             Users
         };
-
         public enum Action
         {
             GET,
@@ -53,5 +98,10 @@ namespace Windows_CLient
             PUT,
             DELETE
         };
+
+        class InvalidActionException : Exception
+        {
+            public InvalidActionException(string message) : base(message) { }
+        }
     }
 }
