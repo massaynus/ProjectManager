82ddd712e2611d7a1728d536684b6319031042fc
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-06-03 23:19:05 +0100

    Layed foundation for Teams and Workers View Models (Managers Area)

--- Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/TeamsViewModel.cs
+++ Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/TeamsViewModel.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows_CLient.Models;
 
 namespace Windows_CLient.ViewModels
 {
     public class TeamsViewModel : BaseViewModel
     {
         //TODO: complete the TeamsViewModel
+        //TODO: write commands
+        public ObservableCollection<Team> Teams { get; set; }
+        public Team SelectedTeam { get; set; }
+
+        public ICommand GetTeams { get; set; }
+        public ICommand AddTeam { get; set; }
+        public ICommand UpdateTeam { get; set; }
+        public ICommand GetTeams { get; set; }
     }
 }
--- Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/WorkersViewModel.cs
+++ Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/WorkersViewModel.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows_CLient.Models;
 
 namespace Windows_CLient.ViewModels
 {
     public class WorkersViewModel : BaseViewModel
     {
         //TODO: complete the WorkersViewModel
+        //TODO: Write commands
+        public ObservableCollection<User> Users { get; set; }
+        public ObservableCollection<Team> Teams { get; set; }
+        public User SelectedUser { get; set; }
+        public ICommand GetUsers { get; set; }
+        public ICommand GetTeams { get; set; }
+        public ICommand ResetPassword { get; set; }
+        public ICommand UpdateUser { get; set; }
+        public ICommand DisableUserAccount { get; set; }
+        public ICommand EnableUserAccount { get; set; }
+
     }
 }
