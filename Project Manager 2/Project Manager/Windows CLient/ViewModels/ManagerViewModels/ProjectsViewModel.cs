a08fbb67b50848a1f452e714676b820327e95dd4
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-06-07 19:24:34 +0100

    Finished Projects View and ViewModel for managers Area

--- Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/ProjectsViewModel.cs
+++ Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/ProjectsViewModel.cs
@@ -8,6 +8,7 @@ using System.Windows.Input;
 using Windows_CLient.Models;
 using Newtonsoft.Json;
 using System.Windows;
+using static Windows_CLient.APIClient;
 
 namespace Windows_CLient.ViewModels
 {
@@ -16,17 +17,23 @@ namespace Windows_CLient.ViewModels
         private Project selectedProject = new Project();
         public ProjectsViewModel()
         {
+            Teams = new List<Team>();
             Projects = new ObservableCollection<Project>();
 
             GetProjects = new RelayCommandAsync(getProjetcs);
-            AssigneProject = new RelayCommandAsync(assigneProject);
+            GetTeams = new RelayCommandAsync(getTeams);
+
+            UpdateProject = new RelayCommandAsync(assigneProject);
             AddProject = new RelayCommandAsync(addProject);
-            NewProject = new RelayCommand(() => SelectedProject = new Project());
+            NewProject = new RelayCommand(newProject);
 
             GetProjects.Execute(null);
+            if (Teams.Count == 0) GetTeams.Execute(null);
         }
 
         public ObservableCollection<Project> Projects { get; set; }
+        public List<Team> Teams { get; set; }
+
         public Project SelectedProject 
         { 
             get => selectedProject;
@@ -41,11 +48,16 @@ namespace Windows_CLient.ViewModels
         }
 
         public ICommand GetProjects { get; set; }
-        public ICommand AssigneProject { get; set; }
+        public ICommand GetTeams { get; set; }
+        public ICommand UpdateProject { get; set; }
         public ICommand AddProject { get; set; }
         public ICommand NewProject { get; set; }
 
-
+        async TT.Task getTeams()
+        {
+            Teams = JsonConvert.DeserializeObject<List<Team>>(await MakeApiCall(APIClient.Action.GET, Controller.Teams));
+            OnPropertyChanged(nameof(Teams));
+        }
         async TT.Task getProjetcs()
         {
             var res = await APIClient.client.GetAsync("Projects");
@@ -63,16 +75,10 @@ namespace Windows_CLient.ViewModels
         }
         async TT.Task assigneProject()
         {
-            var content = APIClient.GetStringContent(SelectedProject);
-            var res = await APIClient.client.PutAsync("Projects", content);
-            if (res.IsSuccessStatusCode)
-            {
-                MessageBox.Show($"Project was assigned successfuly!!!", "Great!", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            else
-            {
-                MessageBox.Show($"Status Code: {res.StatusCode}\n{res.ReasonPhrase}", "Oops!", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
+            string content = await MakeApiCall(APIClient.Action.PUT, Controller.Projects, SelectedProject, SelectedProject.ProjectID.ToString());
+            if (string.IsNullOrEmpty(content)) MessageBox.Show("Project was assigned successfuly!!!", "Great!", MessageBoxButton.OK, MessageBoxImage.Information);
+            else MessageBox.Show($"Project wasn't assigned successfuly", "Oops!", MessageBoxButton.OK, MessageBoxImage.Warning);
+
         }
         async TT.Task addProject()
         {
@@ -86,6 +92,11 @@ namespace Windows_CLient.ViewModels
             {
                 MessageBox.Show($"Status Code: {res.StatusCode}\n{res.ReasonPhrase}", "Oops!", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
+        }
+        void newProject()
+        {
+            SelectedProject = new Project() { Owner = APIClient.User.UserID };
+            Projects.Add(SelectedProject);
         }
     }
 }
