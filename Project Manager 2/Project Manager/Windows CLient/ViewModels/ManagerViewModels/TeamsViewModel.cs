2237e431a1914b7ee104c608436fe5bf00c295ea
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2020-06-07 18:25:00 +0100

    Fixed ObservableCollection not updating bug

--- Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/TeamsViewModel.cs
+++ Project Manager 2/Project Manager/Windows CLient/ViewModels/ManagerViewModels/TeamsViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using TT = System.Threading.Tasks;
@@ -9,12 +10,14 @@ using System.Windows.Input;
 using Windows_CLient.Models;
 using static Windows_CLient.APIClient;
 using System.Windows;
+using System.Reflection;
 
 namespace Windows_CLient.ViewModels
 {
     public class TeamsViewModel : BaseViewModel
     {
         private Team selectedTeam = new Team();
+        private List<User> users = new List<User>();
         public TeamsViewModel()
         {
             Teams = new ObservableCollection<Team>();
@@ -47,11 +50,27 @@ namespace Windows_CLient.ViewModels
         public ICommand DisassembleTeam { get; set; }
         public ICommand NewTeam { get; set; }
 
-
         async TT.Task getTeams()
         {
+            if (users.Count == 0) users = JsonConvert.DeserializeObject<List<User>>(await MakeApiCall(APIClient.Action.GET, Controller.Users));
+
             Teams = JsonConvert.DeserializeObject<ObservableCollection<Team>>(await MakeApiCall(APIClient.Action.GET, Controller.Teams));
+            users.ForEach(u =>
+            {
+                var team = Teams.Where(T => T.TeamID == u.Team).FirstOrDefault();
+                if (team.Users is null) team.Users = new List<User>();
+                team.Users.Add(u);
+            });
+
+            var No_Team = Teams.Where(T => T.Name == "No Team").FirstOrDefault();
+            if (No_Team.Users is null || No_Team.Users?.Count == 0) Teams.Remove(No_Team);
+
             OnPropertyChanged(nameof(Teams));
+            if (Teams.Count > 0)
+            {
+                SelectedTeam = Teams[0];
+                OnPropertyChanged(nameof(SelectedTeam));
+            }
         }
         async TT.Task addTeams()
         {
@@ -70,9 +89,8 @@ namespace Windows_CLient.ViewModels
         async TT.Task updateTeams()
         {
             string JSON = await MakeApiCall(APIClient.Action.PUT, Controller.Teams, SelectedTeam, SelectedTeam.TeamID.ToString());
-            if (JSON.Contains("TeamID"))
+            if (string.IsNullOrEmpty(JSON))
             {
-                SelectedTeam = JsonConvert.DeserializeObject<Team>(JSON);
                 OnPropertyChanged(nameof(SelectedTeam));
                 MessageBox.Show("Team Updated successfuly!!", "Great!", MessageBoxButton.OK, MessageBoxImage.Information);
             }
@@ -84,7 +102,7 @@ namespace Windows_CLient.ViewModels
         async TT.Task disassembleTeam()
         {
             string Names = string.Empty;
-            
+
             var Users = JsonConvert.DeserializeObject<List<User>>(await MakeApiCall(APIClient.Action.GET, Controller.Users));
             Users = Users.Where(U => U.Team == SelectedTeam.TeamID).ToList();
             Users.ForEach(U => Names += $"{U.LastName}\t{U.FirstName}\n");
@@ -97,14 +115,19 @@ namespace Windows_CLient.ViewModels
                 foreach (var user in Users)
                 {
                     user.Team = null;
-                    affectedCount += (await MakeApiCall(APIClient.Action.PUT, Controller.Users, user, user.UserID.ToString())).Contains("UserID") ? 1 : 0;
+                    affectedCount += string.IsNullOrEmpty((await MakeApiCall(APIClient.Action.PUT, Controller.Users, user, user.UserID.ToString()))) ? 1 : 0;
                 }
                 if (affectedCount == Users.Count) MessageBox.Show("Every one was kicked out!!", "Great! >_<", MessageBoxButton.OK, MessageBoxImage.Information);
                 else MessageBox.Show("Something went wrong with kicking some people!!", "Oops!", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
             else MessageBox.Show("Opeation canceled.", "Ok");
         }
-        void newTeam() => SelectedTeam = new Team();
+        void newTeam()
+        {
+            SelectedTeam = new Team();
+            Teams.Add(SelectedTeam);
+            OnPropertyChanged(nameof(Teams));
+        }
 
 
     }
